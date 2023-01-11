dd439973a18883d6805fa09391b69e56bd7b4feb
Vidhya Mani <vidhyamaniece@gmail.com>
2023-01-11 14:47:09 +0530

    Crud requests from postman

--- database/app/controllers/property_controller.rb
+++ database/app/controllers/property_controller.rb
@@ -1,2 +1,35 @@
 class PropertyController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def index 
+        render json: Property.all
+    end
+    def show 
+        v = Property.find(params[:id])
+        render json: v
+    end
+    def create
+        puts params[:propertyname]
+        puts params[:country]
+        puts params[:priceperday]
+        puts params[:rating]
+        v = !params[:propertyname].empty?
+        if (v)
+            p = Property.create('propertyname': params[:propertyname], 'city': params[:city], 'country': params[:country], 'address': params[:address], 'typeofresidence': params[:typeofresidence], 'priceperday': params[:priceperday], 'description': params[:description], 'rules': params[:rules], 'rating': params[:rating])
+            puts p
+            render json: "Data Added"
+        else
+            render json: "Data not added" 
+        end
+    end
+    def update 
+        p = Property.find(params[:id].to_i)
+        p.update('propertyname': params[:propertyname], 'city': params[:city], 'country': params[:country], 'address': params[:address], 'typeofresidence': params[:typeofresidence], 'priceperday': params[:priceperday], 'description': params[:description], 'rules': params[:rules], 'rating': params[:rating])
+        render json: "Data Updated"
+    end
+    def destroy 
+        p = Property.find(params[:id])
+        p.destroy
+        render json: "Data Deleted"
+    end
 end
